7a240b6c5c3e190892e40f91700323be14e2e92f
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-12-22 16:51:12 +0700

    add domain layer

---
+++ lib/features/daily_news/domain/repository/article_repository.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_news_app_clean_architecture_example/core/resources/data_state.dart';
+import 'package:flutter_news_app_clean_architecture_example/features/daily_news/domain/entities/article.dart';
+
+abstract class ArticleRepository {
+  // API methods
+  Future<DataState<List<ArticleEntity>>> getNewsArticles();
+
+  // Database methods
+  Future<List<ArticleEntity>> getSavedArticles();
+
+  Future<void> saveArticle(ArticleEntity article);
+
+  Future<void> removeArticle(ArticleEntity article);
+}
